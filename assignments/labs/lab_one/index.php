f14319c47baccc00f6f4ef97b7841a7d00f98266
Aquiles <200628218@student.georgianc.on.ca>
2026-01-17 16:54:33 -0500

    updated reflection

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -8,4 +8,10 @@ require "header.php";
 require "car.php";  
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+
+/*
+I found creating the car class was pretty enjoyable, but I think i have a good concept of object oriented
+programming. Creating the database connection was a little more tricky and I had to refer to my notes from 
+class.
+*/
 ?>
